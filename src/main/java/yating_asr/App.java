345804db39643dcf54e98e4c4254ca9029e4229b
pinke <pinke.yu7@gmail.com>
2023-08-16 15:20:11 +0800

    Feature: microphone handler

--- src/main/java/yating_asr/App.java
+++ src/main/java/yating_asr/App.java
@@ -4,7 +4,42 @@ import java.net.URI;
 
 public class App {
     public static void main(String[] args) {
-        file();
+        microphone();
+    }
+
+    public static void microphone() {
+        String asrWebSocketUrl = "ASR_WEBSOCKET_ENDPOINT";
+        String asrApiUrl = "ASR_ENDPOINT";
+        String asrApiKey = "PUT_YOUR_API_KEY_HERE";
+
+        String pipeline = Pipeline.General;
+        String language = Language.ZHEN;
+
+        try {
+            // Initialize token client, get auth token
+            AsrTokenClient client = new AsrTokenClient(asrApiUrl, asrApiKey);
+            String authToken = client.GetToken(pipeline, language);
+            System.out.println("authToken: " + authToken);
+
+            // Initialize streaming client, ready for asr recognization
+            AsrStreamingClient asrStreamingClient = new AsrStreamingClient(new URI(asrWebSocketUrl + "?token=" +
+                    authToken));
+            asrStreamingClient.connect();
+
+            boolean scanning = true;
+            while (scanning) {
+                if (asrStreamingClient.isOpen()) {
+                    scanning = false;
+                }
+                Thread.sleep(100);
+            }
+
+            // Microphone audio recognization
+            MicrophoneHandler microphoneHandler = new MicrophoneHandler();
+            microphoneHandler.recognization(asrStreamingClient);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 
     public static void file() {
@@ -14,7 +49,7 @@ public class App {
 
         String filePath = "PUT_YOUR_FILE_PATH_HERE";
         String pipeline = Pipeline.General;
-        String language = Language.ZHEN_OFFLINE;
+        String language = Language.ZHEN;
 
         try {
             // Initialize token client, get auth token
---
+++ src/main/java/yating_asr/MicrophoneHandler.java
@@ -0,0 +1,60 @@
+package yating_asr;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+public class MicrophoneHandler {
+    static float sampleRate = 16000;
+    static int sampleSizeInBits = 16;
+    static int channels = 1;
+
+    static AudioFormat getAudioFormat() {
+        boolean signed = true;
+        boolean bigEndian = false;
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
+                channels, signed, bigEndian);
+        return format;
+    }
+
+    public MicrophoneHandler() {
+
+    }
+
+    public void recognization(AsrStreamingClient asrStreamingClient) throws Exception {
+        AudioFormat format = getAudioFormat();
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+        // checks if system supports the data line
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.println("Line not supported");
+            System.exit(0);
+        }
+        TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+
+        line.open(format);
+        line.start(); // start capturing
+
+        System.out.println("Start capturing...");
+
+        AudioInputStream ais = new AudioInputStream(line);
+
+        System.out.println("Start recording...");
+
+        System.out.println("sampleRate: " + sampleRate);
+        System.out.println("sampleSizeInBits: " + sampleSizeInBits);
+        System.out.println("channels: " + channels);
+
+        final int bytesPerMsec = (int) sampleRate * sampleSizeInBits / 8 / 1000;
+        System.out.println("bytesPerMsec: " + bytesPerMsec);
+
+        while (true) {
+            Thread.sleep(100);
+            byte[] data = new byte[ais.available()];
+            ais.read(data, 0, data.length);
+            asrStreamingClient.send(data);
+        }
+    }
+}
